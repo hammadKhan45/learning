8527d488fe392b211ac7fd41572faadb5b77d13d
hammad Khan <hammad.masood@fliclicks.com>
2016-08-26 11:13:50 +0500

    Add edit and show links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+
+
   def new
     @article = Article.new
 
@@ -14,8 +21,27 @@ class ArticlesController < ApplicationController
    end
   end
 
+  def edit
+@article = Article.find(params[:id])
+  end
+  def update
+
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updates successfully"
+      redirect_to article_path(@article)
+
+    else
+      render "edit"
+
+
+
+    end
+  end
+
+
 def show
- 
+
   @article = Article.find(params[:id])
 end
 
