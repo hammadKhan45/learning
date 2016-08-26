2f79bf67bb5d3cba0cf3ed15e3be92df7360be3f
hammad Khan <hammad.masood@fliclicks.com>
2016-08-26 11:46:46 +0500

    deploying to production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_param_white, only: [:edit, :update, :show, :destroy]
 
   def index
     @articles = Article.all
@@ -22,11 +23,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-@article = Article.find(params[:id])
+
   end
   def update
 
-    @article = Article.find(params[:id])
+
     if @article.update(article_params)
       flash[:notice] = "Article was updates successfully"
       redirect_to article_path(@article)
@@ -42,17 +43,20 @@ class ArticlesController < ApplicationController
 
 def show
 
-  @article = Article.find(params[:id])
+
 end
 
   def destroy
-  @article = Article.find(params[:id])
+
     @article.destroy
   flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
   end
 
   private
+  def set_param_white
+    @article = Article.find(params[:id])
+  end
   def article_params
     params.require(:article).permit(:title, :description)
   end
