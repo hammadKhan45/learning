f32bd72ce29a9c94ede5bebc08ba07ae19fbc287
hammad Khan <hammad.masood@fliclicks.com>
2016-08-29 13:30:45 +0500

    add secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ before_save {self.email = email.downcase }
   validates :email, presence: true, length: {maximum: 105},
       uniqueness: {case_sensitive: false},
       format: {with: VALID_EMAIL_REGEX }
-
+has_secure_password
 
 
 end
